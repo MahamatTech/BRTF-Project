f2d777f7772d4e3a667bbfc5d08d2c077e48945b
madhavjoshi1999 <92283355+madhavjoshi1999@users.noreply.github.com>
2022-02-02 19:37:24 -0500

    d

--- BrtfProject/Models/ReservationRoomDetails.cs
+++ BrtfProject/Models/ReservationRoomDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace BrtfProject.Models
 {
     public class ReservationRoomDetails
     {
+        public int id { get; set; }
+
+        public int ReservationId { get; set; }
+
+        [Display(Name = "Number of rooms booked")]
+        [Required(ErrorMessage = "You cannot leave the room name blank.")]
+        public int NumberOfRoomsBooked { get; set; }
+
+        [Display(Name = "Reservation status")]
+        [Required(ErrorMessage = "You cannot leave the room name blank.")]
+        public string ReservationStatus { get; set; }
+        
+
     }
 }
--- BrtfProject/Models/RoomRules.cs
+++ BrtfProject/Models/RoomRules.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace BrtfProject.Models
 {
     public class RoomRules
     {
+
+        public int id { get; set; }
+        public int RoomId { get; set; }
+
+        [Display(Name = "Rule Name")]
+        [Required(ErrorMessage = "You cannot leave the room name blank.")]
+        [StringLength(100, ErrorMessage = "Room name cannot be more than 100 characters long.")]
+        public string RuleName { get; set; }
+
+        [Display(Name = "Room Name")]
+        public string RuleDescription { get; set; }
+        
     }
 }
