d725073adb94adfd22cf61ddb60be34f57d242ec
madhavjoshi1999 <92283355+madhavjoshi1999@users.noreply.github.com>
2022-02-03 15:59:37 -0500

    d

--- BrtfProject/Data/BRMigrations/20220203203929_Initial.cs
+++ BrtfProject/Data/BRMigrations/20220203203929_Initial.cs
@@ -23,36 +23,6 @@ namespace BrtfProject.Data.BRMigrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "ReservationRoomDetails",
-                columns: table => new
-                {
-                    id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    ReservationId = table.Column<int>(nullable: false),
-                    NumberOfRoomsBooked = table.Column<int>(nullable: false),
-                    ReservationStatus = table.Column<string>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ReservationRoomDetails", x => x.id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "RoomRules",
-                columns: table => new
-                {
-                    id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    RoomId = table.Column<int>(nullable: false),
-                    RuleName = table.Column<string>(maxLength: 100, nullable: false),
-                    RuleDescription = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_RoomRules", x => x.id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
@@ -95,8 +65,8 @@ namespace BrtfProject.Data.BRMigrations
                 {
                     ID = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
-                    room_ID = table.Column<int>(nullable: false),
-                    User_ID = table.Column<int>(nullable: false),
+                    RoomId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
                     StartdateTime = table.Column<DateTime>(nullable: false),
                     EndDateTime = table.Column<DateTime>(nullable: false),
                     BookerID = table.Column<int>(nullable: false)
@@ -105,14 +75,14 @@ namespace BrtfProject.Data.BRMigrations
                 {
                     table.PrimaryKey("PK_Bookings", x => x.ID);
                     table.ForeignKey(
-                        name: "FK_Bookings_Rooms_room_ID",
-                        column: x => x.room_ID,
+                        name: "FK_Bookings_Rooms_RoomId",
+                        column: x => x.RoomId,
                         principalTable: "Rooms",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
-                        name: "FK_Bookings_Users_room_ID",
-                        column: x => x.room_ID,
+                        name: "FK_Bookings_Users_RoomId",
+                        column: x => x.RoomId,
                         principalTable: "Users",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
@@ -127,35 +97,119 @@ namespace BrtfProject.Data.BRMigrations
                     StartDate = table.Column<DateTime>(nullable: false),
                     EndDate = table.Column<DateTime>(nullable: false),
                     ReservedBy = table.Column<string>(maxLength: 50, nullable: false),
-                    room_ID = table.Column<int>(nullable: false),
-                    User_ID = table.Column<int>(nullable: false)
+                    RoomId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Reservations", x => x.id);
                     table.ForeignKey(
-                        name: "FK_Reservations_Rooms_room_ID",
-                        column: x => x.room_ID,
+                        name: "FK_Reservations_Rooms_RoomId",
+                        column: x => x.RoomId,
                         principalTable: "Rooms",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
-                        name: "FK_Reservations_Users_room_ID",
-                        column: x => x.room_ID,
+                        name: "FK_Reservations_Users_RoomId",
+                        column: x => x.RoomId,
                         principalTable: "Users",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Restrict);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Bookings_room_ID",
-                table: "Bookings",
-                column: "room_ID");
+            migrationBuilder.CreateTable(
+                name: "Room_usage",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    MiddleName = table.Column<string>(maxLength: 30, nullable: true),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(maxLength: 10, nullable: false),
+                    Program = table.Column<string>(maxLength: 30, nullable: true),
+                    Term = table.Column<string>(maxLength: 30, nullable: true),
+                    Usertype = table.Column<string>(maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    RoomID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room_usage", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Room_usage_Rooms_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomRules",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RoomId = table.Column<int>(nullable: false),
+                    RuleName = table.Column<string>(maxLength: 100, nullable: false),
+                    RuleDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomRules", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_RoomRules_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReservationRoomDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReservationId = table.Column<int>(nullable: false),
+                    NumberOfRoomsBooked = table.Column<int>(nullable: false),
+                    ReservationStatus = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationRoomDetails", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ReservationRoomDetails_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Reservations_room_ID",
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationRoomDetails_ReservationId",
+                table: "ReservationRoomDetails",
+                column: "ReservationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_RoomId",
                 table: "Reservations",
-                column: "room_ID");
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Room_usage_RoomID",
+                table: "Room_usage",
+                column: "RoomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomRules_RoomId",
+                table: "RoomRules",
+                column: "RoomId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Rooms_BookingID",
@@ -180,7 +234,7 @@ namespace BrtfProject.Data.BRMigrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_Bookings_Rooms_room_ID",
+                name: "FK_Bookings_Rooms_RoomId",
                 table: "Bookings");
 
             migrationBuilder.DropTable(
@@ -190,10 +244,13 @@ namespace BrtfProject.Data.BRMigrations
                 name: "ReservationRoomDetails");
 
             migrationBuilder.DropTable(
-                name: "Reservations");
+                name: "Room_usage");
 
             migrationBuilder.DropTable(
                 name: "RoomRules");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
 
             migrationBuilder.DropTable(
                 name: "Rooms");
